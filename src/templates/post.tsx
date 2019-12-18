21c040606ffcf8512db5c22012208302e9c8d9dc
이태훈 <taehun.lee@monolith.co.kr>
2019-12-18 20:31:12 +0900

    [TEMP]

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -208,6 +208,7 @@ export interface PageContext {
 
 const PageTemplate: React.FC<PageTemplateProps> = props => {
   const post = props.data.markdownRemark;
+  console.log(props);
   let width = '';
   let height = '';
   if (post.frontmatter.image && post.frontmatter.image.childImageSharp) {
